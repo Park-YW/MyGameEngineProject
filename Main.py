6dd1233f9bec6abcd5f31d566409d3ed35784755
Park-YW <156740720+Park-YW@users.noreply.github.com>
2024-12-08 02:56:36 +0900

    multiple render complete

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from Object import *
 
-camera_pos = [0.0, 0.0, 5.0]  # 카메라 위치
+camera_pos = [0.0, 5.0, 5.0]  # 카메라 위치
 camera_target = [0.0, 0.0, 0.0]  # 카메라가 바라보는 점
 camera_up = [0.0, 1.0, 0.0]  # 업 벡터
 
@@ -20,7 +20,7 @@ white = pygame.Color(255, 255, 255)
 glMatrixMode(GL_PROJECTION)
 gluPerspective(60, (screen_width / screen_height), 0.1, 100.0)
 glMatrixMode(GL_MODELVIEW)
-glTranslatef(0.0, 0.0, -3.0)
+#glTranslatef(0.0, 0.0, -3.0)
 glEnable(GL_DEPTH_TEST)
 #glEnable(GL_LIGHTING)
 
@@ -29,13 +29,21 @@ glEnable(GL_DEPTH_TEST)
 #glLightfv(GL_LIGHT0, GL_DIFFUSE, (1, 1, 0, 1))
 #glLightfv(GL_LIGHT0, GL_SPECULAR, (0, 1, 0, 1))
 
+gluLookAt(camera_pos[0], camera_pos[1], camera_pos[2],
+              camera_target[0], camera_target[1], camera_target[2],
+              camera_up[0], camera_up[1], camera_up[2])
+
 glEnable(GL_LIGHT0)
 
 # Change path name to suit your directory structure
 meshes = []
 mesh = Cube()
 #meshes.append(mesh)
-meshes.append(Object())
+point1 = Cube()
+point1.Scale(1)
+point1.Translation((0, 0 ,1))
+#meshes.append(point1)
+
 
 while not done:
     for event in pygame.event.get():
@@ -45,22 +53,23 @@ while not done:
     
     key = pygame.key.get_pressed()
     if key[pygame.K_w]:
-        mesh.Rotation('x',5)
-        mesh.Trasition((0,0,0.01))
+        mesh.Rotation('z',5)
+        #mesh.Translation((0,0,0.1))
     if key[pygame.K_s]:
         mesh.Rotation('x',-5)
-        mesh.Trasition((0,0,-0.01))
+        #mesh.Translation((0,0,-0.01))
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     
-    gluLookAt(camera_pos[0], camera_pos[1], camera_pos[2],
-              camera_target[0], camera_target[1], camera_target[2],
-              camera_up[0], camera_up[1], camera_up[2])
+    
     
     #glRotatef(5, 1, 0, 1)
+    
+    #glBegin(GL_POLYGON)
     for models in meshes:
         models.draw()
     mesh.draw()
+    #glEnd()
 
     pygame.display.flip()
     pygame.time.wait(50)
--- Object.py
+++ Object.py
@@ -2,6 +2,7 @@ from OpenGL.GL import *
 from pyrr import matrix44, quaternion, vector4
 #from numpy.matrixlib import *
 from numpy import *
+from random import *
 
 
 
@@ -47,22 +48,43 @@ class Object:
         self.outputBuffer = [arr[:] for arr in self.vertices]
         self.trangles = [0, 2, 3, 0, 3, 1, ]
         
-        self.rotationBuffer = IDENTITY
-        self.transitionBuffer = IDENTITY
+        self.rotationBuffer = matrix([[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1.0]])
+        self.translationBuffer = matrix([[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1.0]])
+        self.ScaleBuffer = matrix([[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1.0]])
+        self.Color = (1,0,0)
         
-    def Rotation(self, axis, degree):
+    def Rotation(self, axis = 'x', degree = 0):
         Rmatrix = IDENTITY
         
-        if axis == 'x':
+        if axis == 'z': 
             Rmatrix = matrix([  [cos(deg2rad(degree)), sin(deg2rad(degree)), 0, 0],
                                 [-sin(deg2rad(degree)), cos(deg2rad(degree)), 0, 0],
                                 [0, 0, 1, 0],
+                                [0, 0, 0, 1.0]])
+        if axis == 'x': 
+            Rmatrix = matrix([  [1, 0, 0, 0],
+                                [0, cos(deg2rad(degree)), sin(deg2rad(degree)), 0],
+                                [0, -sin(deg2rad(degree)), cos(deg2rad(degree)), 0],
+                                [0, 0, 0, 1.0]])
+        if axis == 'y': 
+            Rmatrix = matrix([  [cos(deg2rad(degree)), 0, sin(deg2rad(degree)), 0],
+                                [0, 1, 0, 0],
+                                [-sin(deg2rad(degree)), 0, cos(deg2rad(degree)), 0],
                                 [0, 0, 0, 1.0]])
         
         self.rotationBuffer *= Rmatrix
         
         
-    def Trasition(self, coord = (0, 0, 0)):
+    def Translation(self, coord = (0, 0, 0)):
         if len(coord) != 3:
             print("AtoM Input Error")
         else:
@@ -70,17 +92,29 @@ class Object:
                                 [0, 1, 0, coord[1]],
                                 [0, 0, 1, coord[2]],
                                 [0, 0, 0, 1.0]])
-            self.transitionBuffer *= Tmatrix
+            self.translationBuffer *= Tmatrix
+            
+    def Scale(self, size=1.):
+        Smatrix = matrix([  [size, 0, 0, 0],
+                            [0, size, 0, 0],
+                            [0, 0, size, 0],
+                            [0, 0, 0, 1.0]])
+        self.ScaleBuffer *= Smatrix
+        
+    def ColorSet(self, color = (1,0,0)):
+        self.Color = color
 
     def draw(self):
         
         for i in range(0, len(self.vertices)):
-            
-            self.outputBuffer[i] = ReturnHomo(self.rotationBuffer@ToHomo(self.vertices[i]))
+            self.outputBuffer[i] = ReturnHomo(self.ScaleBuffer@ToHomo(self.vertices[i]))
             self.outputBuffer[i] = ReturnHomo(self.rotationBuffer@ToHomo(self.outputBuffer[i]))
+            self.outputBuffer[i] = ReturnHomo(self.translationBuffer@ToHomo(self.outputBuffer[i]))
+            
             
         for t in range(0, len(self.trangles), 3):
             glBegin(GL_POLYGON)
+            glColor3f(self.Color[0], self.Color[1], self.Color[2])
             glVertex3fv(self.outputBuffer[self.trangles[t]])
             glVertex3fv(self.outputBuffer[self.trangles[t + 1]])
             glVertex3fv(self.outputBuffer[self.trangles[t + 2]])
